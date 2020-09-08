1fba53035e5e1cc864640f2ea57c048e4307c878
vithall <1061651220@qq.com>
2020-09-08 13:23:25 +0800

    Signed-off-by: vithall <1061651220@qq.com>

---
+++ src/main/java/com/project/SeaForce/Ingredient.java
@@ -0,0 +1,15 @@
+package com.project.SeaForce;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Ingredient {
+	private final String id;
+	private final String name;
+	private final Type type;
+	public static enum Type {
+		WRAP,PROTEIN,VEGGIES
+	}
+}
---
+++ src/main/java/com/project/SeaForce/SeaForceApplication.java
@@ -0,0 +1,13 @@
+package com.project.SeaForce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaForceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaForceApplication.class, args);
+	}
+
+}
